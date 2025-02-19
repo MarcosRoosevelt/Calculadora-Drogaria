c71123456ad5b17dc0efa2942ef8697b9ec4fe46
Marcos Roosevelt <63478082+MarcosRoosevelt@users.noreply.github.com>
2025-02-19 06:54:40 -0300

    all set with the design and the script

---
+++ script.js
@@ -0,0 +1,18 @@
+function calcular() {
+  const medida = parseFloat(document.getElementById("medida-input").value);
+  const peso = parseFloat(document.getElementById("peso-input").value);
+  const soro = parseFloat(document.getElementById("soro-input").value);
+  const ml = parseFloat(document.getElementById("ml-input").value);
+
+  const outcome = medida + peso + soro + ml;
+
+  document.getElementById("outcome-div").innerHTML = outcome + " mcg/kg/min";
+}
+
+function zerarInputs() {
+  document.getElementById("medida-input").value = "";
+  document.getElementById("peso-input").value = "";
+  document.getElementById("soro-input").value = "";
+  document.getElementById("ml-input").value = "";
+  document.getElementById("outcome-div").innerHTML = "";
+}
