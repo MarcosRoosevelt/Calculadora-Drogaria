6e4249b441ae7d5028a8853a3be81d66f07d016f
Marcos Roosevelt <63478082+MarcosRoosevelt@users.noreply.github.com>
2025-02-25 08:41:19 -0300

    Adding the unit of measure

--- script.js
+++ script.js
@@ -2,13 +2,15 @@ function calcular() {
   const medida = parseFloat(document.getElementById("medida-input").value);
   const peso = parseFloat(document.getElementById("peso-input").value);
   const soro = parseFloat(document.getElementById("soro-input").value);
+  const soro_unit = document.getElementById("soro-unit-input").value;
   const ml = parseFloat(document.getElementById("ml-input").value);
 
   const concentration = soro / ml;
   const concentration__by_flow_rate = concentration * medida;
   const outcome = concentration__by_flow_rate / peso;
-  
-  document.getElementById("outcome-div").innerHTML = outcome + " mcg/kg/min";
+
+  document.getElementById("outcome-div").innerHTML =
+    outcome.toFixed(2) + " " + soro_unit + "/kg/h";
 }
 
 function zerarInputs() {
